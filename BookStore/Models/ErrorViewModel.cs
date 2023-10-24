d6ceb78afdd09634a5780888ed586411bee6fb04
vituthanh <vituthanh@gmail.com>
2023-10-25 06:45:32 +0700

    feat(data): Create ApplicationDbContext

--- BookStore/Models/ErrorViewModel.cs
+++ BookStore/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace BookStore.Models
 {
 	public class ErrorViewModel
 	{
-		public string? RequestId { get; set; }
+		public string RequestId { get; set; }
 
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 	}
